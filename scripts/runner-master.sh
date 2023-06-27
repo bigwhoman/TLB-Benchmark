176cd78e7d933bf338705659f9658340f4be41a1
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2023-06-27 20:33:58 +0330

    Re-structure files Started postgres report

--- scripts/runner-master.sh
+++ scripts/runner-master.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 perf record -o out.perf -e tlb:tlb_flush,dTLB-loads,dTLB-load-misses,iTLB-load-misses,cache-misses,page-faults ls
-strace 2> strace.log
+strace -f ls 2> strace.log
 perf script -i out.perf
