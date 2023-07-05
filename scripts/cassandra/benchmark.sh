465c26dc7c36733e2e49617fcfbee4a686c69bbc
bigwhoman <hm.keshvari@gmail.com>
2023-07-06 01:36:18 +0330

    added cassandra documentations(starting)

--- scripts/cassandra/benchmark.sh
+++ scripts/cassandra/benchmark.sh
@@ -1,10 +1,10 @@
-sudo perf record -o ./benchmark/cassandra-benchmark-3-7-bare-metal-$(uname -r).perf -e tlb:tlb_flush,dTLB-loads,dTLB-load-misses,iTLB-load-misses,cache-misses,page-faults cassandra-stress mixed ratio\(write=3,read=7\) n=1000000 -rate threads=50 &> output.txt
-# strace cassandra-stress mixed ratio\(write=3,read=7\) n=1000000 -rate threads=50 2> ./benchmark/cassandra-benchmark-3-7-bare-metal-$(uname -r).strace 
+sudo perf record -o ./benchmark/cassandra-benchmark-3-7-bare-metal-$(uname -r).perf -e tlb:tlb_flush,dTLB-loads,dTLB-load-misses,iTLB-load-misses,cache-misses,page-faults cassandra-stress mixed ratio\(write=3,read=7\) n=1000000 -rate threads=50
+strace cassandra-stress mixed ratio\(write=3,read=7\) n=1000000 -rate threads=50 2> ./benchmark/cassandra-benchmark-3-7-bare-metal-$(uname -r).strace 
 
 
-# sudo perf record -o ./benchmark/cassandra-benchmark-5-5-bare-metal-$(uname -r).perf -e tlb:tlb_flush,dTLB-loads,dTLB-load-misses,iTLB-load-misses,cache-misses,page-faults cassandra-stress mixed ratio\(write=3,read=7\) n=1000000 -rate threads=50
-# strace cassandra-stress mixed ratio\(write=5,read=5\) n=1000000 -rate threads=50 2> ./benchmark/cassandra-benchmark-5-5-bare-metal-$(uname -r).strace 
+sudo perf record -o ./benchmark/cassandra-benchmark-5-5-bare-metal-$(uname -r).perf -e tlb:tlb_flush,dTLB-loads,dTLB-load-misses,iTLB-load-misses,cache-misses,page-faults cassandra-stress mixed ratio\(write=3,read=7\) n=1000000 -rate threads=50
+strace cassandra-stress mixed ratio\(write=5,read=5\) n=1000000 -rate threads=50 2> ./benchmark/cassandra-benchmark-5-5-bare-metal-$(uname -r).strace 
 
 
-# sudo perf record -o ./benchmark/cassandra-benchmark-7-3-bare-metal-$(uname -r).perf -e tlb:tlb_flush,dTLB-loads,dTLB-load-misses,iTLB-load-misses,cache-misses,page-faults cassandra-stress mixed ratio\(write=3,read=7\) n=1000000 -rate threads=50
-# strace cassandra-stress mixed ratio\(write=7,read=3\) n=1000000 -rate threads=50 2> ./benchmark/cassandra-benchmark-7-3-bare-metal-$(uname -r).strace 
+sudo perf record -o ./benchmark/cassandra-benchmark-7-3-bare-metal-$(uname -r).perf -e tlb:tlb_flush,dTLB-loads,dTLB-load-misses,iTLB-load-misses,cache-misses,page-faults cassandra-stress mixed ratio\(write=3,read=7\) n=1000000 -rate threads=50
+strace cassandra-stress mixed ratio\(write=7,read=3\) n=1000000 -rate threads=50 2> ./benchmark/cassandra-benchmark-7-3-bare-metal-$(uname -r).strace 
